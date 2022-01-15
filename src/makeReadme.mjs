14a6329258a02aad63256062133e2725dfe8ac08
ramiTae <ramiTae.dev@gmail.com>
2022-01-15 14:22:57 +0900

    feature: READEME 생성 모듈 추가

---
+++ src/makeReadme.mjs
@@ -0,0 +1,46 @@
+import { readdir, lstat, copyFile, open } from 'fs/promises';
+
+export async function makeReadme() {
+    const dirList = await readdir('./');
+
+    const fileDataList = (await Promise.all(dirList.map(async dir => {
+        if (dir[0] === '.') {
+            return;
+        }
+        if (dir === 'asset' || dir === 'src') {
+            return;
+        }
+
+        const path = `./${dir}`;
+        const stats = await lstat(path);
+        if (!stats.isDirectory()) {
+            return;
+        }
+
+        const fileList = await readdir(path);
+        if (!fileList.length) {
+            return;
+        }
+
+        return { dir, fileList };
+    }))).filter(Boolean);
+
+    await copyFile('./asset/README.template.md', './README.md');
+    const filehandle = await open('./README.md', 'a');
+
+    const text = fileDataList.reduce((acc, val) => {
+        acc += `## ${val.dir}\n`;
+        val.fileList.forEach(file => {
+            const subject = file.replace(/-/g,' ').replace('.md','');
+            const path = `./${val.dir}/${file}`;
+
+            acc += `* [${subject}](${path})\n`
+        })
+
+        return acc;
+    }, '\n')
+
+    await filehandle.appendFile(text);
+}
+
+makeReadme()
