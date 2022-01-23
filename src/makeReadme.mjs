4d50f85fe7e6d8e42ded69570694ae02c5231522
ramiTae <ramiTae.dev@gmail.com>
2022-01-24 00:23:24 +0900

    refactor: Change by prettier

--- src/makeReadme.mjs
+++ src/makeReadme.mjs
@@ -1,47 +1,51 @@
-import { readdir, lstat, copyFile, open } from 'fs/promises';
+import { readdir, lstat, copyFile, open } from "fs/promises";
 
 export async function makeReadme() {
-    const dirList = await readdir('./');
+  const dirList = await readdir("./");
 
-    const fileDataList = (await Promise.all(dirList.map(async dir => {
-        if (dir[0] === '.') {
-            return;
+  const fileDataList = (
+    await Promise.all(
+      dirList.map(async (dir) => {
+        if (dir[0] === ".") {
+          return;
         }
-        if (dir === 'asset' || dir === 'src') {
-            return;
+        if (dir === "asset" || dir === "src") {
+          return;
         }
 
         const path = `./${dir}`;
         const stats = await lstat(path);
         if (!stats.isDirectory()) {
-            return;
+          return;
         }
 
         const fileList = await readdir(path);
         if (!fileList.length) {
-            return;
+          return;
         }
 
         return { dir, fileList };
-    }))).filter(Boolean);
+      })
+    )
+  ).filter(Boolean);
 
-    await copyFile('./asset/template/README.md', './README.md');
-    const filehandle = await open('./README.md', 'a');
+  await copyFile("./asset/template/README.md", "./README.md");
+  const filehandle = await open("./README.md", "a");
 
-    const text = fileDataList.reduce((acc, val) => {
-        acc += `## ${val.dir}\n`;
-        val.fileList.forEach(file => {
-            const subject = file.replace(/-/g,' ').replace('.md','');
-            const path = `/${val.dir}/${file}`;
+  const text = fileDataList.reduce((acc, val) => {
+    acc += `## ${val.dir}\n`;
+    val.fileList.forEach((file) => {
+      const subject = file.replace(/-/g, " ").replace(".md", "");
+      const path = `/${val.dir}/${file}`;
 
-            acc += `* [${subject}](${path})\n`
-        })
+      acc += `* [${subject}](${path})\n`;
+    });
 
-        return acc;
-    }, '\n')
+    return acc;
+  }, "\n");
 
-    console.log(text)
-    await filehandle.appendFile(text);
+  console.log(text);
+  await filehandle.appendFile(text);
 }
 
-makeReadme()
+makeReadme();
